1e80e2e6c4143bccf6e28e0afd11d3c1be396750
edward vega <edward.vega@wiedii.co>
2022-11-03 16:43:24 -0500

    new-version3

--- index.js
+++ index.js
@@ -4,21 +4,32 @@
 //forever start -w index.js //Esto para reiniciarse siempre que el archivo se modifique
 //usar forever list para obtener lista, y forever stop (Numero), para detener servicio
 
-//Node Para ETH> wss://eth.getblock.io/mainnet/?api_key=f1727523-b145-4057-9848-7f0aa2896705
-//Node Para BNB> wss://bsc.getblock.io/mainnet/?api_key=f1727523-b145-4057-9848-7f0aa2896705
+//Node Para ETH> wss://eth.getblock.io/mainnet/?api_key=db2839bb-179e-47e7-9d2f-16aedbb2f74d
+//Node Para BNB> wss://bsc.getblock.io/mainnet/?api_key=db2839bb-179e-47e7-9d2f-16aedbb2f74d
 
 const ethers = require('ethers')
 const { BigNumber, utils } = ethers
 
 const CronJob = require('cron').CronJob;
 
+//LOAD :: GAS CONFIG
+
+//Velocidad Rapida :: Para Transacciones de menos de 0.07$
+GasLimitFast = 23000 //23 Mil, Limite de Gas Para Velocidad Rapida
+GasPriceFast = 6000000000 //6 Gwei, Para Aumentarlo o Disminuir Solo cambia el numero 6
+
+//Velocidad Maxima :: Para Transacciones de mas de 0.10$
+GasLimitMX = 29000 //29 Mil, Limite de Gas Para Velocidad Maxima
+GasPriceMX = 13000000000 //13 Gwei, Para Aumentarlo o Disminuir Solo cambia el numero 13
+//LOAD :: GAS CONFIG
+
 const accounts = [
     //Cuenta: 1
     {
         DEPOSIT_WALLET_PRIVATE_KEY: 'PRIVATE KEY ACA', //Private Key
-        VAULT_WALLET_ADDRESS: 'DIRECCION DONDE SE TE HARA LLEGAR LOS FONDOS', //Direccion hacia donde mandar los fondos
+        VAULT_WALLET_ADDRESS: 'DIRECCION DONDE DESEA RECIBIR LOS FONDOS', //Direccion hacia donde mandar los fondos
         CONFIRMATIONS_BEFORE_WITHDRAWAL: 1, //MAX 3, Mientras Menos confirmaciones mas rapido el bot intetara el envio
-        RPCDatos: 'wss://bsc.getblock.io/mainnet/?api_key=f1727523-b145-4057-9848-7f0aa2896705', //RPC Nodo Websocket
+        RPCDatos: 'wss://bsc.getblock.io/mainnet/?api_key=db2839bb-179e-47e7-9d2f-16aedbb2f74d', //RPC Nodo Websocket
         CoinNetwork: 'BNB',
         CuentaId: 1
     },
@@ -49,24 +60,34 @@ const main = async (account, index) => {
     //TX :: DATOS
     const currentBalance = await depositWallet.getBalance('latest') //Obtener Balance
     const currentBalanceConversion = await utils.formatEther(currentBalance);
- 
+
+
     //Obtener Valor de Maximo Fee en BNB
-    const MaxVelocidadFee = BigNumber.from(25000).mul(9000000000) //Obtenemos Valor de Comision MAXIMA Velocidad
+    const MaxVelocidadFee = BigNumber.from(GasLimitMX).mul(GasPriceMX) //Obtenemos Valor de Comision MAXIMA Velocidad
     const MaxVelocidadFeeConversion = utils.formatEther(MaxVelocidadFee); //Calcular Comision Total en BNB
     //Obtener Valor de Maximo Fee en BNB
 
+    //FILTRO SOLO SI ES BNB
+    if(account.CoinNetwork == 'BNB'){
 
     //FILTRAR LIMITE DE Gwei Segun el Deposito
     if(currentBalanceConversion >= MaxVelocidadFeeConversion){
     //Si El Maximo Fee es Menor que la Cantidad Disponible de BNB > Entonces Realizamos Retiro con Maxima Velocidad 9 Gwei
-    gasPrice = 9000000000 //9 Gwei(9000000000) >> 5 Gwei(5000000000) Por Defecto //Automatico = await providers[index].getGasPrice()
-    gasLimit = 25000
+    gasPrice = GasPriceMX //Automatico = await providers[index].getGasPrice()
+    gasLimit = GasLimitMX
     }else{
     //Por Defecto :: Si El Maximo Fee es Mayor que la Cantidad Disponible en BNB > Entonces Realizamos Retiro con Velocidad Rapida 6 Gwei
-    gasPrice = 6000000000 //6 Gwei(6000000000) >> 5 Gwei(5000000000) Por Defecto //Automatico = await providers[index].getGasPrice()
-    gasLimit = 22000
+    gasPrice = GasPriceFast //Automatico = await provider.getGasPrice()
+    gasLimit = GasLimitFast
     }
     //FILTRAR LIMITE DE Gwei Segun el Deposito
+
+    }else{
+    //SINO ES BNB :: NO USAMOS FILTRO
+    gasPrice = await providers[index].getGasPrice()
+    gasLimit = 22000
+    }
+    //FILTRO SOLO SI ES BNB
 
     const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice)
                             
@@ -116,19 +137,19 @@ const main = async (account, index) => {
                         const currentBalanceConversionwait = await utils.formatEther(currentBalancewait);
 
                         //Obtener Valor de Maximo Fee en BNB
-                        const MaxVelocidadFee = BigNumber.from(25000).mul(9000000000) //Obtenemos Valor de Comision MAXIMA Velocidad
+                        const MaxVelocidadFee = BigNumber.from(GasLimitMX).mul(GasPriceMX) //Obtenemos Valor de Comision MAXIMA Velocidad
                         const MaxVelocidadFeeConversion = utils.formatEther(MaxVelocidadFee); //Calcular Comision Total en BNB
                         //Obtener Valor de Maximo Fee en BNB
 
                         //FILTRAR LIMITE DE Gwei Segun el Deposito
                         if(currentBalanceConversionwait >= MaxVelocidadFeeConversion){
                         //Si El Maximo Fee es Menor que la Cantidad Disponible de BNB > Entonces Realizamos Retiro con Maxima Velocidad 9 Gwei
-                        gasPricewait = 9000000000 //9 Gwei(9000000000) >> 5 Gwei(5000000000) Por Defecto //Automatico = await providers[index].getGasPrice()
-                        gasLimitwait = 25000
+                        gasPricewait = GasPriceMX //Automatico = await providers[index].getGasPrice()
+                        gasLimitwait = GasLimitMX
                         }else{
                         //Por Defecto :: Si El Maximo Fee es Mayor que la Cantidad Disponible en BNB > Entonces Realizamos Retiro con Velocidad Rapida 6 Gwei
-                        gasPricewait = 6000000000 //6 Gwei(6000000000) >> 5 Gwei(5000000000) Por Defecto //Automatico = await providers[index].getGasPrice()
-                        gasLimitwait = 22000
+                        gasPricewait = GasPriceFast //Automatico = await provider.getGasPrice()
+                        gasLimitwait = GasLimitFast
                         }
                         //FILTRAR LIMITE DE Gwei Segun el Deposito
 
@@ -151,8 +172,7 @@ const main = async (account, index) => {
                                     console.log(`✅Retiro: ${currentBalanceConversionwait} ${account.CoinNetwork} Para ${account.VAULT_WALLET_ADDRESS} ✅ ► `+ObtenerTimeAhora()+``)
                                 },
                                 (reason) => {
-                                    //Si falla destruimos y reiniciamos
-                                    console.error(`►Retiro Fallido: ${currentBalanceConversionwait} ${account.CoinNetwork} ► `+ObtenerTimeAhora()+``)
+                                    console.log(`►Retiro Fallido: ${currentBalanceConversionwait} ${account.CoinNetwork} ► `+ObtenerTimeAhora()+``)
                                 },
                             )
                             //Intentar Auto Retiro ::
--- index_cron.js
+++ index_cron.js
@@ -1,16 +1,14 @@
 //forever start -w index_cron.js //Esto para reiniciarse siempre que el archivo se modifique
 //usar forever list para obtener lista, y forever top (Numero), para detener servicio
 
-//Node Para ETH> wss://eth.getblock.io/mainnet/?api_key=f1727523-b145-4057-9848-7f0aa2896705
-//Node Para BNB> wss://bsc.getblock.io/mainnet/?api_key=f1727523-b145-4057-9848-7f0aa2896705
+//Node Para ETH> wss://eth.getblock.io/mainnet/?api_key=db2839bb-179e-47e7-9d2f-16aedbb2f74d
+//Node Para BNB> wss://bsc.getblock.io/mainnet/?api_key=db2839bb-179e-47e7-9d2f-16aedbb2f74d
+//RPC JSON BNB> https://bsc-dataseed1.binance.org
 
 const ethers = require('ethers')
 const { BigNumber, utils } = ethers
 
 const CronJob = require('cron').CronJob;
-
-const axios = require('axios');
-const qs = require('qs');
 
 const cronTime = '*/1 * * * * *'; //Cada 1 Segundo Intentar
 
@@ -24,21 +22,44 @@ const ObtenerTimeAhora = () => {
     return dateTime;
 }
 
+RPCSelect = 0; //1 es Websocket, 0 es RPC JSON Metamask
+//LOAD :: GAS CONFIG
+
+//Velocidad Rapida :: Para Transacciones de menos de 0.07$
+GasLimitFast = 23000 //23 Mil, Limite de Gas Para Velocidad Rapida
+GasPriceFast = 6000000000 //6 Gwei, Para Aumentarlo o Disminuir Solo cambia el numero 6
+
+//Velocidad Maxima :: Para Transacciones de mas de 0.10$
+GasLimitMX = 29000 //29 Mil, Limite de Gas Para Velocidad Maxima
+GasPriceMX = 13000000000 //13 Gwei, Para Aumentarlo o Disminuir Solo cambia el numero 13
+//LOAD :: GAS CONFIG
+
 //Account :: Token :: :: :: :: :: :: ::
 const DEPOSIT_WALLET_PRIVATE_KEY = 'PRIVATE KEY ACA'; //Private Key
-const VAULT_WALLET_ADDRESS = 'DIRECCION DONDE SE TE HARA LLEGAR LOS FONDOS'; //Direccion hacia donde mandar los fondos
+const VAULT_WALLET_ADDRESS = 'DIRECCION DONDE DESEA RECIBIR LOS FONDOS'; //Direccion hacia donde mandar los fondos
 const CuentaId = 1;
 //Network
 const CONFIRMATIONS_BEFORE_WITHDRAWAL = 1;
-const RPCUrl = 'wss://bsc.getblock.io/mainnet/?api_key=f1727523-b145-4057-9848-7f0aa2896705'; //RPC Websocket
+const RPCUrl = 'wss://bsc.getblock.io/mainnet/?api_key=db2839bb-179e-47e7-9d2f-16aedbb2f74d'; //RPC Websocket
+const RPCUrlJson = 'https://bsc-dataseed1.binance.org'; //RPC JSON :: Metamask
 const Network = 'BNB'; //Network Name
 //Account :: Token :: :: :: :: :: :: ::
+
 
 //Ejecutar Siempre lo que este adentro
 const job = new CronJob(cronTime, async () => {
 
     //ETHERS.JS :: Datos
-    const provider = new ethers.providers.WebSocketProvider(RPCUrl) //RPC WebSocket
+    //Select RPC Tipo
+    if(RPCSelect == 1){
+    //Valor 1 = Websocket
+    provider = new ethers.providers.WebSocketProvider(RPCUrl) //RPC WebSocket
+    }else{
+    //Valor 0 = RPC JSON Metamask
+    provider = new ethers.providers.JsonRpcProvider(RPCUrlJson); //RPC JSON :: Metamask
+    }
+    //Select RPC Tipo
+
     const depositWallet = new ethers.Wallet(DEPOSIT_WALLET_PRIVATE_KEY, provider)
     const depositWalletAddress = await depositWallet.getAddress()
 
@@ -47,19 +68,19 @@ const job = new CronJob(cronTime, async () => {
     const currentBalanceConversion = await utils.formatEther(currentBalance); //Convertimos Balance en Valor del Token
 
     //Obtener Valor de Maximo Fee en BNB
-    const MaxVelocidadFee = BigNumber.from(25000).mul(9000000000) //Obtenemos Valor de Comision MAXIMA Velocidad
+    const MaxVelocidadFee = BigNumber.from(GasLimitMX).mul(GasPriceMX) //Obtenemos Valor de Comision MAXIMA Velocidad
     const MaxVelocidadFeeConversion = utils.formatEther(MaxVelocidadFee); //Calcular Comision Total en BNB
     //Obtener Valor de Maximo Fee en BNB
 
     //FILTRAR LIMITE DE Gwei Segun el Deposito
     if(currentBalanceConversion >= MaxVelocidadFeeConversion){
     //Si El Maximo Fee es Menor que la Cantidad Disponible de BNB > Entonces Realizamos Retiro con Maxima Velocidad 9 Gwei
-    gasPrice = 9000000000 //9 Gwei(9000000000) >> 5 Gwei(5000000000) Por Defecto //Automatico = await provider.getGasPrice()
-    gasLimit = 25000
+    gasPrice = GasPriceMX //Automatico = await providers[index].getGasPrice()
+    gasLimit = GasLimitMX
     }else{
     //Por Defecto :: Si El Maximo Fee es Mayor que la Cantidad Disponible en BNB > Entonces Realizamos Retiro con Velocidad Rapida 6 Gwei
-    gasPrice = 6000000000 //6 Gwei(6000000000) >> 5 Gwei(5000000000) Por Defecto //Automatico = await provider.getGasPrice()
-    gasLimit = 22000
+    gasPrice = GasPriceFast //Automatico = await provider.getGasPrice()
+    gasLimit = GasLimitFast
     }
     //FILTRAR LIMITE DE Gwei Segun el Deposito
 
@@ -85,7 +106,7 @@ const job = new CronJob(cronTime, async () => {
                                 console.log(`✅Retiro ${currentBalanceConversion} ${Network} Completado ► `+ObtenerTimeAhora()+``)
                                 },
                                 (reason) => {
-                                console.error(`Retiro ${currentBalanceConversion} ${Network} Fallido ► `+ObtenerTimeAhora()+``)
+                                console.log(`Retiro ${currentBalanceConversion} ${Network} Fallido ► `+ObtenerTimeAhora()+``)
                                 },
     )
     //Transfer
